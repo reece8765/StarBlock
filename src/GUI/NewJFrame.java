d1a6fc4958d66e3528293d2e54aa5b2b803478ac
Adam <adamjxbox@hotmail.co.uk>
2020-03-07 19:14:06 +0000

    Got buttons, labels and images working

--- src/GUI/NewJFrame.java
+++ src/GUI/NewJFrame.java
@@ -5,6 +5,10 @@
  */
 package GUI;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author reece
@@ -17,6 +21,13 @@ public class NewJFrame extends javax.swing.JFrame {
     public NewJFrame() {
         initComponents();
     }
+    private final NumberFormat formatter = NumberFormat.getCurrencyInstance(Locale.UK);
+
+    String name = "Jack";
+    int ID = 111;
+    double Balance = 5.00;
+    double CurrentBet = 1.00;
+    double IncramentNumber = 0.10;
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -27,21 +38,179 @@ public class NewJFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        BTNSpin = new javax.swing.JButton();
+        DecButton = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        LBLBalance = new javax.swing.JLabel();
+        LBLBet = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/87778015_131282251653696_8106294353615388672_n.jpg"))); // NOI18N
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/87778015_131282251653696_8106294353615388672_n.jpg"))); // NOI18N
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/87778015_131282251653696_8106294353615388672_n.jpg"))); // NOI18N
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/87778015_131282251653696_8106294353615388672_n.jpg"))); // NOI18N
+
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Images/87778015_131282251653696_8106294353615388672_n.jpg"))); // NOI18N
+
+        BTNSpin.setText("Spin!");
+        BTNSpin.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BTNSpinActionPerformed(evt);
+            }
+        });
+
+        DecButton.setText("Decrease Bet");
+        DecButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                DecButtonActionPerformed(evt);
+            }
+        });
+
+        jButton3.setText("Auto");
+
+        jButton4.setText("Increase Bet");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
+        jLabel6.setText("StarBlock");
+
+        jLabel7.setText("Player: " +name);
+
+        LBLBalance.setText("Balance: "+formatter.format(Balance));
+
+        LBLBet.setText("Current Bet: " + formatter.format(CurrentBet));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(58, 58, 58)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel1)
+                            .addComponent(jLabel7))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(DecButton)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jButton3)
+                                    .addComponent(BTNSpin)
+                                    .addGroup(layout.createSequentialGroup()
+                                        .addGap(10, 10, 10)
+                                        .addComponent(LBLBet)))
+                                .addGap(33, 33, 33))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel2)
+                                .addGap(18, 18, 18)
+                                .addComponent(jLabel3)
+                                .addGap(18, 18, 18)))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(jLabel4)
+                                    .addComponent(jButton4))
+                                .addGap(18, 18, 18)
+                                .addComponent(jLabel5))
+                            .addComponent(LBLBalance)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(330, 330, 330)
+                        .addComponent(jLabel6)))
+                .addContainerGap(67, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(29, 29, 29)
+                .addComponent(jLabel6)
+                .addGap(48, 48, 48)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel4)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel3)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(BTNSpin)
+                            .addComponent(LBLBalance))
+                        .addGap(4, 4, 4)
+                        .addComponent(LBLBet)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton3))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel7)
+                        .addGap(12, 12, 12)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(DecButton)
+                            .addComponent(jButton4))))
+                .addContainerGap(68, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void BTNSpinActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BTNSpinActionPerformed
+        // TODO add your handling code here:
+        if (Balance >= CurrentBet) {
+            Balance -= CurrentBet;
+            updateBalance();
+
+            System.out.println("User does have enough currency");
+        } else {
+            JOptionPane.showMessageDialog(null, "Your balance is too low for this bet", "Insufficient Funds", JOptionPane.WARNING_MESSAGE);
+        }
+
+    }//GEN-LAST:event_BTNSpinActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+
+        if (!String.valueOf(formatter.format(CurrentBet)).equals("£10.00")) {
+            CurrentBet += IncramentNumber;
+            updateBet();
+        }
+
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void DecButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DecButtonActionPerformed
+        // TODO add your handling code here:
+        if (!String.valueOf(formatter.format(CurrentBet)).equals("£1.00")) {
+            CurrentBet -= IncramentNumber;
+            updateBet();
+
+        }
+    }//GEN-LAST:event_DecButtonActionPerformed
+
+    private void updateBet() {
+        LBLBet.setText("Current Bet: " + formatter.format(CurrentBet));
+
+    }
+
+    private void updateBalance() {
+        LBLBalance.setText("Balance: " + formatter.format(Balance));
+
+    }
 
     /**
      * @param args the command line arguments
@@ -79,5 +248,18 @@ public class NewJFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BTNSpin;
+    private javax.swing.JButton DecButton;
+    private javax.swing.JLabel LBLBalance;
+    private javax.swing.JLabel LBLBet;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     // End of variables declaration//GEN-END:variables
 }
